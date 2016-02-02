30a026be75066b16daa87a6f3bac6dc5d17eac48
Yifan Zhang <freqyifan@gmail.com>
2016-02-02 10:32:13 +0300

    added option to convert utf8 to buckwalter

--- evaluation/xml2stm.py
+++ evaluation/xml2stm.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# this script is to be used to convert MGB challenge xmls to SCLITE stm
+# format so it can be used as reference for evaluation.
+
 __author__ = 'Yifan Zhang (yzhang@qf.org.qa)'
 
 import os
@@ -6,6 +10,18 @@ import sys
 import time
 import codecs
 from xml.dom import minidom
+
+_unicode = u"\u0622\u0624\u0626\u0628\u062a\u062c\u06af\u062e\u0630\u0632\u0634\u0636\u0638\u063a\u0640\u0642\u0644\u0646\u0648\u064a\u064c\u064e\u0650\u0652\u0670\u067e\u0686\u0621\u0623\u0625\u06a4\u0627\u0629\u062b\u062d\u062f\u0631\u0633\u0635\u0637\u0639\u0641\u0643\u0645\u0647\u0649\u064b\u064d\u064f\u0651\u0671"
+_buckwalter = u"|&}btjGx*z$DZg_qlnwyNaio`PJ'><VApvHdrsSTEfkmhYFKu~{"
+
+_forwardMap = {ord(a):b for a,b in zip(_unicode, _buckwalter)}
+_backwardMap = {ord(b):a for a,b in zip(_unicode, _buckwalter)}
+
+def toBuckWalter(s):
+  return s.translate(_forwardMap)
+
+def fromBuckWalter(s):
+  return s.translate(_backwardMap)
 
 class Element(object):
   def __init__(self, text, startTime, endTime=None):
@@ -22,7 +38,16 @@ def loadXml(xmlFileName, opts):
     sid = segment.attributes['id'].value
     startTime = float(segment.attributes['start'].value)
     endTime = float(segment.attributes['end'].value)
-    text = ' '.join((e.childNodes[0].data for e in segment.getElementsByTagName('element')))
+
+    tokens = [e.childNodes[0].data for e in segment.getElementsByTagName('element')]
+    # skip any word starts with '#'
+    tokens = filter(lambda i: not i.startswith('#'), tokens)
+    # convert to buckwalter if required
+    if opts.buck:
+      tokens = map(toBuckWalter, tokens)
+
+    text = ' '.join(tokens)
+
     elements.append(Element(text, startTime, endTime))
   return {'id': sid, 'turn': elements}
 
@@ -60,6 +85,8 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='convert Arabic MGB xml file to MGB xml')
   parser.add_argument("--id", dest="uid",
                       help="utterance id")
+  parser.add_argument("--buck", dest="buck", default=False, action='store_true',
+                      help="output buckwalter text")
   parser.add_argument("--ctm", dest="ctm", default=False, action='store_true',
                       help="output ctm file for testing")
   parser.add_argument("--skip-bad-segments", dest="skip_bs", default=False, action='store_true',
