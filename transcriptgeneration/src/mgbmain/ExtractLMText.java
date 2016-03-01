eb4af7a9643d2ffcb242d520f6fc81ec716c1ea6
Sameer Khurana <sameerkhurana10@gmail.com>
2016-03-01 13:52:13 +0300

    adding annotation @@LAT to english words and mapping arabic indic to arabic numerals

--- transcriptgeneration/src/mgbmain/ExtractLMText.java
+++ transcriptgeneration/src/mgbmain/ExtractLMText.java
@@ -4,31 +4,30 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileWriter;
-import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 import mgbutils.ArabicUtils;
 import mgbutils.MGBUtil;
 
+/**
+ * 
+ * @author Sameer Khurana (skhurana@qf.org.qa)
+ *
+ */
 public class ExtractLMText {
 
 	public static void main(String[] args) {
 
 		try {
-                    String out, msg;
-                    int fileNo, errors;
-			//File[] xmlFiles = new File("/Users/alt-sameerk/Documents/aj_net/ARTICLES").listFiles(new FileFilter() {
-                        File[] xmlFiles = new File("D:\\PERL\\aljazeera.net_archive\\ARTICLES").listFiles(new FileFilter() {
+			File[] xmlFiles = new File("/Users/alt-sameerk/Documents/aj_net/ARTICLES").listFiles(new FileFilter() {
 
 				@Override
 				public boolean accept(File pathname) {
@@ -36,13 +35,7 @@ public class ExtractLMText {
 				}
 			});
 
-			fileNo = 0;
-                        errors = 0;
-                        for (File fxmlFile : xmlFiles) {
-                            
-                            fileNo++;
-                            msg = String.format("ExtractLMText() for file:%d, errors:%d", fileNo, errors);
-                            System.out.println(msg);
+			for (File fxmlFile : xmlFiles) {
 
 				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -51,13 +44,10 @@ public class ExtractLMText {
 					doc = dBuilder.parse(fxmlFile);
 				} catch (SAXParseException e) {
 					System.out.println("not a good document cannot be parsed");
-                                        errors++;
 					continue;
 				}
 				doc.getDocumentElement().normalize();
-				// System.out.println("Root element :" +
-				// doc.getDocumentElement().getNodeName());
-				int c = 0;
+
 				NodeList nList1 = doc.getElementsByTagName("doc");
 				BufferedWriter bw = null;
 				for (int j = 0; j < nList1.getLength(); j++) {
@@ -68,14 +58,36 @@ public class ExtractLMText {
 						Node nNode = nList.item(i);
 						if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 							Element element = (Element) nNode;
-							if (element.getAttribute("name").equals("Content") || element.getAttribute("name").equals("mitemMainTitle")) {
-								String content = MGBUtil.normalizeWord(element.getTextContent()
+							if (element.getAttribute("name").equals("Content")) {
+								String content = ArabicUtils.normalize(element.getTextContent()
 										.replaceAll("[،؟:/!,؛\"]", "").replaceAll("[^0-9٠-٩]\\.[^0-9٠-٩]", "")
 										.replaceAll("[\\(\\)\\[\\]_]", "").replaceAll("\\s\\s^\n", " ")
 										.replaceAll("[-\\.$]", "").replaceAll("[A-Za-z]", "")
 										.replaceAll("(?m)^[\t]*\r?\n", ""));
-                                                                out = ArabicUtils.utf82buck(content.trim());
-								bw.write(out + "\n");
+
+								String newContent = "";
+
+								for (String word : content.split(" ")) {
+									if (word.matches("[A-Za-z]+")) {
+										word = "@@LAT@@" + word;
+									} else {
+										word = ArabicUtils.utf82buck(ArabicUtils.removeDiacritics(word));
+									}
+									word.replaceAll("ﻷ", "");
+									word.replaceAll("٩", "9");
+									word.replaceAll("٨", "8");
+									word.replaceAll("٧", "7");
+									word.replaceAll("٦", "6");
+									word.replaceAll("٥", "5");
+									word.replaceAll("٤", "4");
+									word.replaceAll("٣", "3");
+									word.replaceAll("٢", "2");
+									word.replaceAll("١", "1");
+									word.replaceAll("٠", "0");
+
+									newContent = newContent + " " + word;
+								}
+								bw.write(ArabicUtils.utf82buck(content.trim()));
 								bw.close();
 
 							}
@@ -84,14 +96,9 @@ public class ExtractLMText {
 					}
 
 				}
-                                //break;
 			}
 
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
