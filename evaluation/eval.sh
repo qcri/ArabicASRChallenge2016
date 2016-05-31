f8ffd556434395a4c96a8b740e15f0f3b99514bb
Yifan Zhang <freqyifan@gmail.com>
2016-05-31 16:40:20 +0300

    eval script to run evaluation

---
+++ evaluation/eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+EVALSTM=$1    # utf8 reference in stm format
+EVALCTM=$2    # utf8 recognition result in ctm format
+
+if [[ -s arabic_utf8.glm ]]; then
+  GLM="-g arabic_utf8.glm"
+fi
+
+
+hubscr.pl -d -V -f ctm -F stm -l arabic -h hub5 $GLM -r $EVALSTM $EVALCTM
