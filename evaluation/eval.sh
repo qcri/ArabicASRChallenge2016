090897c27a57be3d39d9abb4f93fd135f329eb67
Yifan Zhang <yifan@YifandeMacBook-Pro.local>
2017-01-02 09:10:52 +0300

    made things more logical

--- evaluation/eval.sh
+++ evaluation/eval.sh
@@ -9,16 +9,16 @@ export PATH=$sctk_bin:$PATH
 
 EVALSTM=$1    # utf8 reference in stm format
 EVALCTM=$2    # utf8 recognition result in ctm format
-SKIPUTF8=${3-false}
+BUCKWALTER=${3-false}     # true if input ctm is in buckwalter
 
 if [[ -s arabic_utf8.glm ]]; then
   GLM="-g arabic_utf8.glm"
 fi
 
-if $SKIPUTF8; then
-  cp $EVALCTM $EVALCTM.utf8
-else
+if $BUCKWALTER; then
   ./toutf8.py $EVALCTM $EVALCTM.utf8
+else
+  cp $EVALCTM $EVALCTM.utf8
 fi
 
 hubscr.pl -d -V -f ctm -F stm -l arabic -h hub5 $GLM -r $EVALSTM $EVALCTM.utf8
